4480a6b31452a8a9eb2adc7fb5e071ad1fbf7140
Jayanth <kolleparajayanth@gmail.com>
2025-11-10 17:55:14 +0530

    Tara-The Event Managment

--- Frontend/src/services/auth.js
+++ Frontend/src/services/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE  || "http://localhost:2000/api";
+const API_BASE = import.meta.env.VITE_API_BASE  || "https://tara-kbxn.onrender.com/api";
 
 
 export async function login(email, password) {
